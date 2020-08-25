870bfc45cfaaabd1e68caaf477551d05018fe40f
praphawitPRO <bz_4595@yahoo.com>
2020-08-25 16:13:53 +0700

    Updates

---
+++ precache-manifest.37f631609d10a2248b34d6d4517f32e9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362be45a948b1a25e34cfb02dbdac67d",
+    "url": "/cto_stat_report/index.html"
+  },
+  {
+    "revision": "c7cd3595eb75add7dc68",
+    "url": "/cto_stat_report/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "820f133bb2103a841f64",
+    "url": "/cto_stat_report/static/css/main.39122dab.chunk.css"
+  },
+  {
+    "revision": "c7cd3595eb75add7dc68",
+    "url": "/cto_stat_report/static/js/2.3c06c670.chunk.js"
+  },
+  {
+    "revision": "a2a88b186c6e13ce8b4a80862f9fb227",
+    "url": "/cto_stat_report/static/js/2.3c06c670.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820f133bb2103a841f64",
+    "url": "/cto_stat_report/static/js/main.74172020.chunk.js"
+  },
+  {
+    "revision": "a2b67c3935175cc9a13f",
+    "url": "/cto_stat_report/static/js/runtime-main.22cd6c53.js"
+  },
+  {
+    "revision": "038920cb23de901df4061f00e8b2c18d",
+    "url": "/cto_stat_report/static/media/CPN-Bold 2.038920cb.otf"
+  },
+  {
+    "revision": "038920cb23de901df4061f00e8b2c18d",
+    "url": "/cto_stat_report/static/media/CPN-Bold.038920cb.otf"
+  },
+  {
+    "revision": "273f739e430bbc7da9e559b314970904",
+    "url": "/cto_stat_report/static/media/CPN-Bold.273f739e.ttf"
+  },
+  {
+    "revision": "f81ed2f4849092a48455b30fffca34b7",
+    "url": "/cto_stat_report/static/media/CPN-BoldCondensed.f81ed2f4.otf"
+  },
+  {
+    "revision": "ce9a951f935b4ab6a72efc9feb3f2583",
+    "url": "/cto_stat_report/static/media/CPN-BoldItalic.ce9a951f.otf"
+  },
+  {
+    "revision": "1847053c83fa6bd28e863f658b1e710e",
+    "url": "/cto_stat_report/static/media/CPN-Compressed.1847053c.otf"
+  },
+  {
+    "revision": "8cb1184716cb518cb68d7e1c78065b5d",
+    "url": "/cto_stat_report/static/media/CPN-Condensed.8cb11847.otf"
+  },
+  {
+    "revision": "7054ca142f59c28aa1d3e25f57da1303",
+    "url": "/cto_stat_report/static/media/CPN-ExtraCompressed.7054ca14.otf"
+  },
+  {
+    "revision": "7eb523145c339c6d7692250f2b32c635",
+    "url": "/cto_stat_report/static/media/CPN-Italic.7eb52314.otf"
+  },
+  {
+    "revision": "38b82b6383c003d440f58c2b0a5190f1",
+    "url": "/cto_stat_report/static/media/CPN-Light.38b82b63.otf"
+  },
+  {
+    "revision": "d89a491695947bcd9281dd1e2df06bed",
+    "url": "/cto_stat_report/static/media/CPN-LightItalic.d89a4916.otf"
+  },
+  {
+    "revision": "038fbebdd029f9c1e3418393deca0c09",
+    "url": "/cto_stat_report/static/media/CPN-Regular.038fbebd.otf"
+  },
+  {
+    "revision": "5687c0e26b47c0b5d3ae03377cd7a392",
+    "url": "/cto_stat_report/static/media/cto.5687c0e2.jpg"
+  }
+]);
